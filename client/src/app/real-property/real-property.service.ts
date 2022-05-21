9075c354137c68e32f82151a42d9b206188823a0
Chiantine <cmanigos@gmail.com>
2022-05-21 15:56:37 +0800

    Delete confirm kind of property components

--- client/src/app/real-property/real-property.service.ts
+++ client/src/app/real-property/real-property.service.ts
@@ -169,7 +169,7 @@ export class RealPropertyService {
     let headers = new HttpHeaders();
     headers = headers.set('Authorization', `Bearer ${token}`);
 
-    return this.http.post<IKindOfProperty>(this.baseUrl + 'realProperties/kindOfProperties/' + id, {headers});
+    return this.http.delete<IKindOfProperty>(this.baseUrl + 'realProperties/kindOfProperties/' + id, {headers});
   }
 
   onPhotoFileChange(event: any) {
